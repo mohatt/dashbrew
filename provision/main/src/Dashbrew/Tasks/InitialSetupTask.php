8428f3b38dbed8c1555bc22fff422dc8aa32b854
Mohamed Elkholy <mkh117@gmail.com>
2014-09-23 17:22:56 +0300

    Added code for InitialSetup task

--- provision/main/src/Dashbrew/Tasks/InitialSetupTask.php
+++ provision/main/src/Dashbrew/Tasks/InitialSetupTask.php
@@ -22,7 +22,18 @@ class InitialSetupTask extends Task {
             throw new \Exception("The Config task can only be run by the Provision command.");
         }
 
-        //
+        $lock = '/etc/dashbrew/initial-setup.lock';
+        if(file_exists($lock)){
+            return;
+        }
 
+        $fs = Util::getFilesystem();
+        $initial_config_file = '/vagrant/provision/main/config/config.yaml';
+
+        $fs->copy($initial_config_file, '/vagrant/config/config.yaml', true, 'vagrant');
+        $fs->copy($initial_config_file, '/vagrant/provision/main/etc/config.yaml.old', true, 'vagrant');
+
+        $fs->mkdir(dirname($lock));
+        $fs->touch($lock);
     }
 }
