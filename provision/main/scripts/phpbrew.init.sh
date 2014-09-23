29e21cb8b1c523def58bb6c3816615a098de7540
Mohamed Elkholy <mkh117@gmail.com>
2014-09-24 00:15:30 +0300

    Fixed issue in phpbrew scripts

--- provision/main/scripts/phpbrew.init.sh
+++ provision/main/scripts/phpbrew.init.sh
@@ -3,5 +3,4 @@
 # initiate phpbrew
 export PHPBREW_HOME=/opt/phpbrew
 export PHPBREW_ROOT=/opt/phpbrew
-/usr/bin/phpbrew init >/dev/null
 source ${PHPBREW_ROOT}/bashrc
--- provision/main/scripts/phpbrew.php.switch_sys.sh
+++ provision/main/scripts/phpbrew.php.switch_sys.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $DIR/phpbrew.init.sh
 
-phpbrew off
+phpbrew switch-off
