00ec0e1ecd99df2697648ef87701e12f63ddb260
Mohamed Elkholy <mkh117@gmail.com>
2014-11-02 01:45:51 +0200

    Added code to execute pre/post provisioning scripts

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
+
+function run_scripts() {
+    local dir=$1
+    local dir_path="/vagrant/provision/${dir}"
+
+    if [[ -d ${dir_path} && "$(find ${dir_path} -maxdepth 1 -type f -name '*.sh')" ]]; then
+        echo "[Info] Running ${dir}-provisioning scripts in provision/${dir}"
+
+        find ${dir_path} -maxdepth 1 -type f -name '*.sh' | sort | while read FILENAME; do
+            # /bin/bash "${FILENAME}"
+            ${FILENAME}
+        done
+
+        echo "[Info] Finished running ${dir}-provisioning scripts."
+    fi
+}
+
+# Run the pre-provisioning scripts
+run_scripts pre
+
 # Run dashbrew provisioner
 /vagrant/provision/main/dashbrew provision
+
+# Run the post-provisioning scripts
+run_scripts post
